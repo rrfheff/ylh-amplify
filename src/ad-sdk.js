33b7e95896576eea5a566bc5170b9816f770ee8e
ruanruofan <ruanruofan@bytedance.com>
2022-11-25 15:47:56 +0800

    fix

--- src/ad-sdk.js
+++ src/ad-sdk.js
@@ -1,7 +1,7 @@
 window.TencentGDT = window.TencentGDT || [];
 TencentGDT.push({
   placement_id: '11**99', // String，广告位id
-  appid: 'appid'
+  appid: 'appid',
   type: 'rewardVideo', // String，广告类型，请根据广告类型填写对应的type值
   display_type: 'banner',
   onComplete: function(res) { // Function，广告位初始化回调方法，已激励视频接入为例
